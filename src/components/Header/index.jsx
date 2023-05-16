41a8ffb7cb536f0a4a508dc8815a0e98f3a7b7ef
Matheus Negrini <mndias29@gmail.com>
2023-05-15 21:59:21 -0300

    tenho q fazer o design no figma primeiro senão não sai nd

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Podcast Filmes Clássicos</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "./index.scss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import Header from "./components/Header";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Oba</h1>
-  </React.StrictMode>,
-)
+    <Header />
+  </React.StrictMode>
+);
